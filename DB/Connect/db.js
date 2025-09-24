62468abcbb73401a99fa0c2a4d2cf59a6b52cb48
Prateek <rahuls12153@gmail.com>
2025-09-24 19:07:06 +0530

    Final Build

--- DB/Connect/db.js
+++ DB/Connect/db.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose"
 
-const uri = process.env.MONGO_URI
+const MONGO_URI = process.env.MONGO_URI
 
 const Connect = async () => {
 
-    await mongoose.connect(uri)
+    await mongoose.connect(MONGO_URI)
     .then(() => console.log("DB connected successfully"))
     .catch(() => console.error("Somethign broke while connecting to db"))
 

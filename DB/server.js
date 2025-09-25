d6ed07b3fa3a661b97ed436be2bd5b40c937c880
Prateek <rahuls12153@gmail.com>
2025-09-26 04:23:58 +0530

    Final Build

--- DB/server.js
+++ DB/server.js
@@ -115,8 +115,6 @@ app.post("/UserCheck", async (req, res) => {
     // Set session cookie
     res.cookie("SessionID", SessionID, {
       maxAge: 10 * 60 * 1000, // 10 minutes
-      // httpOnly: true,
-      httpOnly: false,
       secure: true,        // required for SameSite=None
       sameSite: "None",    // allow cross-site cookie
       path: "/",

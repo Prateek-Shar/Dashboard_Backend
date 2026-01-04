f7c6e8ef179a6713db441fad5a9edfb12429380b
Prateek-Shar <147049043+Prateek-Shar@users.noreply.github.com>
2026-01-05 03:21:43 +0530

    Changes in cors

--- DB/server.js
+++ DB/server.js
@@ -25,16 +25,14 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, origin);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) return callback(null, true);
+    return callback(null, false);
   },
   credentials: true,
-  methods : "GET,POST" , 
-  allowedHeaders : ["Content-Type"]
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type"]
 }));
 
 app.set("trust proxy" , 1)

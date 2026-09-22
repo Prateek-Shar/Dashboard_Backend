09ce721f91ead9dc0f11f9da2883ca38414392af
Prateek-Shar <147049043+Prateek-Shar@users.noreply.github.com>
2026-09-22 16:02:57 +0530

    modified getTaskStatus route

--- MiddleWare/auth.js
+++ MiddleWare/auth.js
@@ -6,12 +6,14 @@ const getSessionInfo = async(req , res , next) => {
     const session_id = req.cookies.SessionID;
 
     if (!session_id) {
+        console.log("❌ SessionID NOT FOUND in cookies");
         return res.status(401).json({message : "Un-authorized access , No Session in Cookies"})
     }
 
     const session_exists = await Session.findOne({"SessionID" : session_id})
 
     if (!session_exists) {
+        console.log("❌ SessionID exists in cookie but NOT in DB");
         return res.status(401).json({message : "No Session Exists in DB"})
     }
 
